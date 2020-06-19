eddc8f37e6ab2e87cdbb2246f11de1d1939dceed
Pablo Obando <josepabloobandogonzalez@gmail.com>
2020-06-19 17:29:14 -0600

    Send an email to the user to reset their password

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { randomBytes } = require('crypto')
 const { promisify } = require('util')
+const { transport, makeANiceEmail } = require('../mail')
 
 const createItem = async (parent, args, ctx, info) => {
   // TODO: Check if they are logged in
@@ -99,7 +100,14 @@ const requestReset = async (parent, args, ctx, info) => {
     where: { email: args.email },
     data: { resetToken, resetTokenExpiry },
   })
-  // TODO. Email them that reset token
+  const mailRes = await transport.sendMail({
+    from: 'pablo@pablo.com',
+    to: user.email,
+    subject: 'Your Password Reset Token',
+    html: makeANiceEmail(`Your Password Reset Token is here!
+    \n\n
+    <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+  })
   return { message: 'Thanks!' }
 }
 
